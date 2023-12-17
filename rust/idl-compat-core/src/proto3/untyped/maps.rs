da640a592a92e5144f6d04f99de7049dbf9fb6e0
Jacob Abel <jacobabel@nullpo.dev>
2023-12-16 21:52:33 -0500

    NO-MERGE: drop cc-by-samix

--- rust/idl-compat-core/src/proto3/untyped/maps.rs
+++ rust/idl-compat-core/src/proto3/untyped/maps.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Jacob Abel <jacobabel@nullpo.dev>
 //
-// SPDX-License-Identifier: LGPL-3.0-or-later AND CC-BY-SA-4.0
+// SPDX-License-Identifier: LGPL-3.0-or-later
 //
 // This file is part of IDL-Compat.
 //
@@ -16,15 +16,6 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with IDL-Compat. If not, see <https://www.gnu.org/licenses/>. 
-//
-// IDL-Compat Documentation is licensed under the Creative Commons
-// Attribution-ShareAlike 4.0 International License. This only includes any
-// documentation comments or `#[doc]` attributes present in this file unless
-// explicitly indicated otherwise.
-//
-// You should have received a copy of the Creative Commons
-// Attribution-ShareAlike 4.0 International License along with the IDL-Compat
-// Documentation. If not, see <https://creativecommons.org/licenses/by-sa/4.0/>. 
 
 use crate::proto3::untyped::enums::EnumMessage;
 use crate::proto3::untyped::messages::Message;
